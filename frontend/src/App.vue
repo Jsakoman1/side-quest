546982b485e067e14336e0fedbb6910d053d875e
Josip Sakoman <jsakoman1@gmail.com>
2026-05-03 14:22:42 +0200

    Added basic VUE JS frontend (for AppUser)

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {onMounted, ref} from 'vue'
+import axios from 'axios'
+
+const users = ref([])
+
+onMounted(async () => {
+  const res = await axios.get('http://localhost:8080/app_users')
+  users.value = res.data
+})
+</script>
+
+<template>
+  <div>
+    <h1>Users</h1>
+
+    <div v-for="user in users" :key="user.id"> {{ user.id }} - {{ user.email }} - {{ user.username }}</div>
+  </div>
+</template>
